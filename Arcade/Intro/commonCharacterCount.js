c27f6258801f539f7728d59460ba08d03b1e77ed
HuydDo <huydinhdo@yahoo.com>
2020-08-25 23:01:12 -0600

    added commonCharacterCount

--- Arcade/Intro/commonCharacterCount.js
+++ Arcade/Intro/commonCharacterCount.js
@@ -4,13 +4,16 @@ function commonCharacterCount(s1, s2) {
       let s = String.fromCharCode(i)
       if(s1.includes(s) && s2.includes(s)) com.push(s)
   }
-  
+  console.log(com)
   for(let e of com) {
       let a = 0, b = 0
       for(let v of s1) if(v == e) a++
       for(let v of s2) if(v == e) b++
       c += Math.min(a, b)
   }
-  
+  console.log(c)
   return c
 }
+
+let s1 = "aabcc" ; s2 = "adcaa"
+commonCharacterCount(s1, s2)
