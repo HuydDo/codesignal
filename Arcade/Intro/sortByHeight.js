5e02e8abfa5c1b85dc89d496ac31c2ba03221123
HuydDo <huydinhdo@yahoo.com>
2020-08-26 22:32:56 -0600

    modified sortByHeight

--- Arcade/Intro/sortByHeight.js
+++ Arcade/Intro/sortByHeight.js
@@ -1,3 +1,18 @@
 function sortByHeight(a) {
-
-}
+  for(let i = 0; i < a.length; i++) {
+         for(let j = i + 1; j < a.length; j++) {
+             if(a[i] != -1 && a[j] != -1) {
+                 if(a[i] > a[j]) {
+                     let tmp = a[i];
+                     a[i] = a[j];
+                     a[j] = tmp;
+                 }
+             }
+         }
+     }
+     
+     return a;
+ }
+ 
+let a = [-1, 150, 190, 170, -1, -1, 160, 180]
+sortByHeight(a)
