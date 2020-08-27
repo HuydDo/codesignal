e5368583e87de35c694e5bd5124e93137864e9ec
HuydDo <huydinhdo@yahoo.com>
2020-08-27 11:39:00 -0600

    modified firstDuplicate

--- Arcade/Intro/firstDuplicate.js
+++ Arcade/Intro/firstDuplicate.js
@@ -1,13 +1,18 @@
 function firstDuplicate(a) {
   for (let i = 0; i < a.length; i++){
+    console.log('i: ' + i)
+    console.log ('before a[Math.abs(a[i]) - 1]: ' + a[Math.abs(a[i]) - 1])
       if(a[Math.abs(a[i]) - 1] < 0) 
       {
          
          console.log('Math.abs(a[i]):'+Math.abs(a[i]))
           return Math.abs(a[i])
       }
+      let j = Math.abs(a[i]) - 1
+      console.log('array index: ' + j )
+      console.log('after a[Math.abs(a[i])-1]: ' + -a[Math.abs(a[i])-1])
       a[Math.abs(a[i])-1] = -a[Math.abs(a[i]) - 1]
-      console.log( a[Math.abs(a[i])-1] )
+      
   }
   return -1
 }
