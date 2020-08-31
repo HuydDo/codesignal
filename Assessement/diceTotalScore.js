433d877c0977ef02d90665b007593adc0d071c9b
HuydDo <huydinhdo@yahoo.com>
2020-08-31 13:25:57 -0600

    modified diceTotalScore

--- Assessement/diceTotalScore.js
+++ Assessement/diceTotalScore.js
@@ -1,12 +1,20 @@
 //Given the values of a, b, and c, your task is to calculate and return your total score.
 
 
-function diceTotalScore(a, b, c){
-    if(a === c && b === c && a === b){
-        return 100 * (Math.min(a, b, c))
-    }
-  }
 
+function diceTotalScore(a, b, c) {
+  if (a === b && b === c) {
+      return 1000 * a
+  } else if (a === b || b === c) {
+      return 500 * b
+  } else if (a === c) {
+      return 500 * a
+  } else {
+      let minscore = Math.min(a, b)
+      console.log(minscore)
+      return Math.min(minscore, c) * 100
+  };
+};
 
 //tests:
 console.log(diceTotalScore(5, 5, 5))
